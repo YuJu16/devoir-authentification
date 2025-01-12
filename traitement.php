f7447adab99e476d50f0a19c1eda91adc0c66bf0
gjulia <julia.gari75@ynov.com>
2025-01-12 23:18:30 +0100

    devoir fini

--- traitement.php
+++ traitement.php
@@ -1,14 +1,29 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $pseudo = $_POST['pseudo'] ?? '';
-    $password = $_POST['password'] ?? '';
-    if (empty($pseudo) || empty($password)) {
-        echo "Veuillez remplir tous les champs.";
-    } else {
-        echo "Bonjour $pseudo<br>";
-        echo "Votre mot de passe est $password";
+    if ($_SERVER["REQUEST_METHOD"] != "POST") {
+        if ($_POST["pseudo"]=="" or $_POST["pseudo"]==null and $_POST["password"]=="" or $_POST["password"]==null) {
+            header('Location: ../login.php');
+            exit;
+        }
     }
-} else {
-    echo "Désolé, le formulaire n'a pas été envoyé correctement.";
-}
-?>
+
+    echo 'Bienvenue '.$_POST["pseudo"];
+
+    $conn = mysqli_connect("localhost", "root", "");
+
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $conn = new mysqli("localhost", "root", "", "Client");
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO users (pseudo, password)
+    VALUES ('".$_POST["pseudo"]."','".$_POST["password"]."')";
+
+    if ($conn->query($sql) !== TRUE) {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
